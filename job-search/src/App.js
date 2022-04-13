2b35fa1ceaec1f8d70942ac597430c4bd7c54f32
saqibrazzaq <saqibrazzaq@gmail.com>
2022-04-13 10:45:55 +0500

    job search initial files

---
+++ job-search/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className='container ui'>
+        <h1>Job Search</h1>
+      </div>
+    )
+  }
+}
+
+export default App
