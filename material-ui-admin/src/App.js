b81d9e370b9f5e485afddac18de34ec9955f396c
saqibrazzaq <saqibrazzaq@gmail.com>
2022-05-23 23:55:48 +0500

    menu

---
+++ material-ui-admin/src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import TopMenu from './components/TopMenu/TopMenu';
+
+function App() {
+  return (
+    <Container>
+      <TopMenu></TopMenu>
+    </Container>
+  );
+}
+
+export default App;
